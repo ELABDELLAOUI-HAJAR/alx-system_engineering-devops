ae0cfbbff6e63860142a4f6a2140be922adb41ef
HAJAR <elabdellaoui.hajar@gmail.com>
2023-11-07 17:04:07 +0000

    remove count

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,14 +5,14 @@ Module 2-recurse.py that contains the function recurse
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None, count=0):
+def recurse(subreddit, hot_list=[], after=None):
     """function that queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit. If no results
     are found for the given subreddit, the function should return None
     """
     url = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
-    params = {'limit': 100, 'count': count}
+    params = {'limit': 100}
     if after is not None:
         params['after'] = after
 
@@ -23,7 +23,6 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
             return hot_list
         [hot_list.append(post['data']['title']) for post in posts]
         new_after = r.json().get('data', {}).get('after')
-        count += r.json().get('data', {}).get('dist')
-        return recurse(subreddit, hot_list, new_after, count)
+        return recurse(subreddit, hot_list, new_after)
     else:
         return None
